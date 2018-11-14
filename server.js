603d77593a52d93fc3732ebfaa40ffbbe9413b4a
Tanima Ranjan <tanimaranjan@gmail.com>
2018-11-13 22:09:36 -0500

    update for Heroku

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const hbs = require('hbs')
 
 const fs = require('fs')
 
+const port = process.env.PORT || 3000; 
+
+
 var app = express()
 
 
@@ -66,6 +69,6 @@ app.get('/bad', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log("server is up on port 3000")
+app.listen(port, () => {
+    console.log(`server is up on port ${port}`)
 });
